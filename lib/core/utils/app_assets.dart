530e8ada48e7d1dd1f9527a3680a65ffee04ecb4
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2024-11-25 16:36:17 +0200

    add new extantion ..

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar_2.svg
+  static const String imagesAvatar2 = "assets/images/avatar_2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCardMask
+  /// assets/images/card_mask.svg
+  static const String imagesCardMask = "assets/images/card_mask.svg";
+
+  /// Assets for imagesExpense
+  /// assets/images/expense.svg
+  static const String imagesExpense = "assets/images/expense.svg";
+
+  /// Assets for imagesFrame
+  /// assets/images/Frame.svg
+  static const String imagesFrame = "assets/images/Frame.svg";
+
+  /// Assets for imagesGraph
+  /// assets/images/graph.svg
+  static const String imagesGraph = "assets/images/graph.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesSetting2
+  /// assets/images/setting-2.svg
+  static const String imagesSetting2 = "assets/images/setting-2.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesTransaction
+  /// assets/images/transaction.svg
+  static const String imagesTransaction = "assets/images/transaction.svg";
+
+  /// Assets for imagesWallet2
+  /// assets/images/wallet-2.svg
+  static const String imagesWallet2 = "assets/images/wallet-2.svg";
+}
+
